23477d3830e6faff49fb499f38bb4c52e663a02f
ready-research <72916209+ready-research@users.noreply.github.com>
2023-10-19 17:36:00 +0530

    Delete tests directory

--- tests/cli/example-project/README.md
+++
@@ -1,1 +0,0 @@
-A markdown formatted readme for my big flow project.
--- tests/cli/example-project/flows/hello.py
+++
@@ -1,6 +0,0 @@
-from prefect import flow
-
-
-@flow(name="An important name")
-def my_flow(number: int, message: str = None):
-    pass
